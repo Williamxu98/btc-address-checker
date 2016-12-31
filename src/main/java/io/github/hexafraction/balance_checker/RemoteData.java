ab2bf1f6cafdb506900fc9e0d6d00623098e1f46
Williamxu98 <xuwilliam01@gmail.com>
2016-12-30 20:45:41 -0500

    Ignore

---
+++ src/main/java/io/github/hexafraction/balance_checker/RemoteData.java
@@ -0,0 +1,55 @@
+package io.github.hexafraction.balance_checker;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RemoteData {
+    public static HttpURLConnection getConnection(String url){
+        // System.out.println("URL: "+url);
+        HttpURLConnection hcon = null;
+        try {
+            hcon=(HttpURLConnection)new URL(url).openConnection();
+            hcon.setReadTimeout(30000); // Timeout at 30 seconds
+            hcon.setRequestProperty("User-Agent", "Alien V1.0");
+        } catch (MalformedURLException e) {
+
+        } catch (IOException e) {
+
+        }
+        return hcon;
+    }
+
+
+    /**
+     * A very handy utility method that reads the contents of a URL
+     * and returns them as a String.
+     *
+     * @param url
+     * @return
+     */
+
+    public static String readContents(String url){
+        HttpURLConnection hcon=getConnection(url);
+        if(hcon==null) return null;
+        try{
+            StringBuffer sb=new StringBuffer(128000);
+            String tmp="";
+            BufferedReader br=new BufferedReader(
+                    new InputStreamReader(
+                            hcon.getInputStream()
+                    )
+            );
+            while((tmp=br.readLine())!=null)
+                sb.append(tmp).append("\n");
+            br.close();
+            return sb.toString();
+        }catch(IOException e){
+            return null;
+        }
+    }
+ 
+}
